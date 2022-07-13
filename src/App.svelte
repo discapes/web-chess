af3409837cb0e9250e61d4f5ecbd54375487fa99
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-07-13 20:44:43 +0300

    remove default svelte page, add tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,93 +1,115 @@
 <script>
-  import logo from "./assets/svelte.png";
-  import Counter from "./lib/Counter.svelte";
+  // @ts-ignore
   import { Chessboard, fenToObj, objToFen } from "@discape/chessboardjs";
   import { onMount } from "svelte";
 
-  const config = {
-    position: "start",
-    draggable: true,
-    sparePieces: true,
-    pieceTheme: 'https://unpkg.com/@discape/chessboardjs@1.1.0/dist/img/chesspieces/wikipedia/{piece}.png',
-    spareCounts: {
-      wK: 1,
-      wQ: 1,
-      wR: 1,
-      wB: 1,
-      wN: 1,
-      wP: 1,
-      bK: 1,
-      bQ: 1,
-      bR: 1,
-      bB: 1,
-      bN: 1,
-      bP: 1,
-    },
-    scaleDrag: 1.2,
-    cacheImages: true,
-  };
-
   onMount(() => {
-    let chessboard = Chessboard("myBoard", config);
+    let chessboard = Chessboard("myBoard", {
+      position: "start",
+      draggable: true,
+      sparePieces: true,
+      pieceTheme: "https://unpkg.com/@discape/chessboardjs@1.1.0/dist/img/chesspieces/wikipedia/{piece}.png",
+      spareCounts: {
+        wK: 1,
+        wQ: 1,
+        wR: 1,
+        wB: 1,
+        wN: 1,
+        wP: 1,
+        bK: 1,
+        bQ: 1,
+        bR: 1,
+        bB: 1,
+        bN: 1,
+        bP: 1,
+      },
+      scaleDrag: 1.2,
+      cacheImages: true,
+    });
   });
 </script>
 
 <main>
-  <div id="myBoard" style="width: 400px" />
-
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for the officially supported framework, also powered by Vite!
-  </p>
+  <h1>web-chess</h1>
+  <div class="w-96" id="myBoard" />
 </main>
 
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
+<style lang="postcss">
+  @tailwind base;
+  @tailwind utilities;
 
   main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+    @apply font-sans bg-gradient-to-r from-purple-100 to-pink-100 h-screen overflow-y-auto flex flex-col justify-center items-center p-10;
   }
 
-  img {
+  :global(img) {
     height: 16rem;
     width: 16rem;
   }
 
-  h1 {
+  :global(p){
+    max-width: 30rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  :global(h1) {
     color: #ff3e00;
     text-transform: uppercase;
     font-size: 4rem;
     font-weight: 100;
     line-height: 1.1;
     margin: 2rem auto;
-    max-width: 14rem;
+    max-width: 30rem;
   }
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
+  /* :global(h1) {
+    display: block;
+    font-weight: bold;
+    font-size: 2em;
+    margin-block-start: 0.67em;
+    margin-block-end: 0.67em;
+  } */
+
+  :global(h2) {
+    display: block;
+    font-weight: bold;
+    font-size: 1.5em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
   }
 
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
+  :global(h3) {
+    display: block;
+    font-weight: bold;
+    font-size: 1.17em;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+  }
+  :global(h4) {
+    display: block;
+    font-weight: bold;
+    font-size: 1em;
+    margin-block-start: 1.33em;
+    margin-block-end: 1.33em;
+  }
+  :global(h5) {
+    display: block;
+    font-weight: bold;
+    font-size: 0.83em;
+    margin-block-start: 1.67em;
+    margin-block-end: 1.67em;
+  }
 
-    p {
-      max-width: none;
-    }
+  :global(h6) {
+    display: block;
+    font-weight: bold;
+    font-size: 0.67em;
+    margin-block-start: 2.33em;
+    margin-block-end: 2.33em;
+  }
+
+  :global(a){
+    color: blue;
   }
 </style>
